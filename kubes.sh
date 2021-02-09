dfbe86b4c9c167dcfb9d5a2313e349ef5fc8d38f
vivekram87 <61583051+vivekram87@users.noreply.github.com>
2021-02-09 20:12:45 +0530

    Update kubes.sh

--- kubes.sh
+++ kubes.sh
@@ -1,2 +1,2 @@
 kubeadm join 192.168.31.24:6443 --token v738pm.fzrx1sz040qr59wy \
-    --discovery-token-ca-cert-hash sha256:3eca0a54465867b6e84dcf7eb26e3f58b01aba0f9c8d7da714a86f63e1f4a198
+    --discovery-token-ca-cert-hash sha256:3eca0a54465867b6e84dcf7eb26e3f58b01aba0f9c8d7da714a86f63e1f4a198 --ignore-preflight-errors=all
